fafa5fb9645a2a918b761281296329f30f972713
dbgusWKd <gks1949@naver.com>
2022-06-07 01:26:30 +0900

    회원정보조회

--- Stopwaiting/app/src/main/java/com/example/stopwaiting/activity/MyInfoActivity.java
+++ Stopwaiting/app/src/main/java/com/example/stopwaiting/activity/MyInfoActivity.java
@@ -1,4 +1,45 @@
 package com.example.stopwaiting.activity;
 
-public class MyInfoActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.stopwaiting.databinding.MyinfoBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyInfoActivity extends AppCompatActivity {
+    public static Activity myInfoActivity;
+
+    private MyinfoBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = MyinfoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        myInfoActivity = MyInfoActivity.this;
+
+        binding.txtUserName.setText(DataApplication.currentUser.getName());
+        binding.txtUserNum.setText(Math.toIntExact(DataApplication.currentUser.getStudentCode()));
+        binding.txtUserPhone.setText(DataApplication.currentUser.getTel());
+
+//        MyInfoRequest();
+    }
+
 }
